58783263b9521924ac46b867d69c89d71bfbe788
Efe_Tamay_Kuzu <efetamaykuzu@gmail.com>
2024-05-19 19:07:02 +0300

    ilk gorevde textler haric her sey hazir.

---
+++ Assets/Kodlar/AlveBirakBasit.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AlveBirakBasit : MonoBehaviour
+{
+    public GameObject odun;
+    public Transform hedef2;
+    public GameObject KampAtesi;
+    public GameObject isaret;
+    public GameObject Portal;
+    private bool b = false;
+    private bool adam = false;
+    public string sahneismi;
+
+    private void Start()
+    {
+        odun.SetActive(true);
+        isaret.SetActive(true);
+        KampAtesi.SetActive(false);
+        Portal.SetActive(false);
+        
+    }
+
+    private void Update()
+    {
+        if (adam == true)
+        {
+            odun.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Nesne"))
+        {
+            odun.SetActive(false);
+            isaret.SetActive(false);
+            b = true;
+        }
+
+        if (other.CompareTag("Kamp"))
+        {
+            if (b == true)
+            {
+                adam = true;
+                if (adam == true)
+                {
+                    odun.SetActive(true);  // odun'u tekrar etkinleþtiriyoruz
+                    odun.transform.position = hedef2.transform.position;
+                    KampAtesi.SetActive(true);
+                    Portal.SetActive(true);
+                }
+            }
+        }
+        if (other.CompareTag("Portal"))
+        {
+            SceneManager.LoadScene(sahneismi);
+        }
+    }
+}
